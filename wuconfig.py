5251f197fb47de3c5967f332fae710d886ac990c
lbehrler <38298678+lbehrler@users.noreply.github.com>
2021-01-18 20:17:07 -0600

    Update wuconfig.py

--- wuconfig.py
+++ wuconfig.py
@@ -1,4 +1,4 @@
 class Config:
     # Weather Underground
-    STATION_ID = "KTXMANSF125"
-    STATION_KEY = "VpAEW0HV"
+    STATION_ID = "KTXMANSF125"  #Personalize this to YOUR weather station
+    STATION_KEY = "VpAEW0HV"    #Personalize this to YOUR weather station
