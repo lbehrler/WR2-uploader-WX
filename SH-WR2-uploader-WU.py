0c51eb2e288f4790e035a5234ee1a5a51077c6f0
lbehrler <bacn@eeul.com>
2021-01-20 21:46:06 -0600

    Added PWSweather.com upload to script

--- SH-WR2-uploader-WU.py
+++ SH-WR2-uploader-WU.py
@@ -20,7 +20,7 @@ import urllib
 
 from sense_hat import SenseHat
 
-from wuconfig import Config
+from config import Config
 
 try:
     from urllib import urlencode
@@ -33,13 +33,11 @@ except ImportError:
 
 DEBUG_MODE = True
 # specifies how often to measure values from the Sense HAT (in minutes)
-MEASUREMENT_INTERVAL = 10  # seconds
+PWS_INTERVAL = 2  # minutes
 # Set to False when testing the code and/or hardware
 # Set to True to enable upload of weather data to Weather Underground
 WEATHER_UPLOAD = True
-# some string constants
-# SINGLE_HASH = '#'
-# HASHES = '############################################'
+
 
 # constants used to display an up and down arrows plus bars
 # modified from https://www.raspberrypi.org/learning/getting-started-with-the-sense-hat/worksheet/
@@ -96,11 +94,20 @@ plus = [
 # last_temp = 0
 wu_station_id = ''
 wu_station_key = ''
+pws_station_id = ''
+pws_station_key = ''
 sense = None
 shMsg = ''
 failct = 0
 goodct = 0
 
+# initialize the lastMinute variable to the current time to start
+last_minute = dt.datetime.now().minute
+# on init, just use the previous minute as lastMinute
+last_minute -= 1
+if last_minute == 0:
+    last_minute = 59
+    logging.debug('Last Minute: {}'.format(last_minute))
 
 # Setup the basic console logger
 format_str = '%(asctime)s %(levelname)s %(message)s'
@@ -116,28 +123,50 @@ logging.basicConfig(format=format_str, level=logging.INFO, datefmt=date_format)
 
 #  Read Weather Underground Configuration
 logging.info('Initializing Weather Underground configuration')
-wu_station_id = Config.STATION_ID
-wu_station_key = Config.STATION_KEY
+wu_station_id = Config.WU_STATION_ID
+wu_station_key = Config.WU_STATION_KEY
 if (wu_station_id == "") or (wu_station_key == ""):
     logging.error('Missing values from the Weather Underground configuration file')
     sys.exit(1)
 
+#  Read PWSweather.com  Configuration
+logging.info('Initializing PWSweather.com configuration')
+pws_station_id = Config.PWS_STATION_ID
+pws_station_key = Config.PWS_STATION_KEY
+if (pws_station_id == "") or (pws_station_key == ""):
+    logging.error('Missing values from the PWS Weather configuration file')
+    sys.exit(1)
+
 # we made it this far, so it must have worked...
 logging.info('Successfully read Weather Underground configuration')
-logging.info('Station ID: {}'.format(wu_station_id))
+logging.info('WU Station ID: {}'.format(wu_station_id))
 logging.debug('Station key: {}'.format(wu_station_key))
+logging.info('Successfully read PWSweather configuration')
+logging.info('Station ID: {}'.format(pws_station_id))
+logging.debug('Station key: {}'.format(pws_station_key))
 
+date_str = "&dateutc=now"  #Default date stamp for weather services
+
+
+# Weather Underground URL formation --------
 # Create a string to hold the first part of the URL
 # Standard upload
 #WUurl = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?"
-#action_str = "&action=updateraw"
+#WUaction_str = "&action=updateraw"
 
 # Rapid fire server
 WUurl = "https://rtupdate.wunderground.com/weatherstation/updateweatherstation.php?"
-action_str = "&realtime=1&rtfreq=16"
+WUaction_str = "&realtime=1&rtfreq=16"
 
 WUcreds = "ID=" + wu_station_id + "&PASSWORD="+ wu_station_key
-date_str = "&dateutc=now"
+
+
+# PWS weather URL formation --------
+# Create a string to hold the first part of the URL
+PWSurl = "https://pwsweather.com/weatherstation/updateweatherstation.php?"
+PWSaction_str = "&action=updateraw"
+
+PWScreds = "ID=" + pws_station_id + "&PASSWORD="+ pws_station_key
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 # initialize the Sense HAT object
@@ -215,7 +244,7 @@ t.start()
 pulse = 0
 while True:
     #   Other processing can occur here as needed...
-    logging.info('Made it to processing step. \n')
+    logging.info('Looking for WR2 data')
 
     try:
         src, line = q.get(timeout = 1)
@@ -228,10 +257,10 @@ while True:
         #print(sLine)
         #   See if the data is something we need to act on...
         if (( sLine.find('F007TH') != -1) or ( sLine.find('F016TH') != -1)):
-            logging.info('WeatherSense Indoor T/H F016TH Found' + '\n')
-            logging.info('raw data: ' + sLine + '\n')
+            logging.info('WeatherSense Indoor T/H F016TH Found')
+            logging.info('raw data: ' + sLine)
             # Variable Processing from JSON output from Indoor T/H unit for WU upload
-            logging.info('Variable processing of Indoor T/H raw data. \n')
+            logging.info('Variable processing of Indoor T/H raw data.')
             raw_data = json.loads(sLine)
             indhumidity_str = "{0:.0f}".format(raw_data['humidity'])
             indtemp_str =  "{0:.1f}".format(raw_data['temperature_F'])
@@ -243,13 +272,13 @@ while True:
             # clear the screen
             sense.clear()
         if (( sLine.find('FT0300') != -1) or ( sLine.find('FT020T') != -1)):
-            logging.info("WeatherSense WeatherRack2 FT020T found' + '\n")
-            logging.info('raw data: ' + sLine + '\n')
+            logging.info('WeatherSense WeatherRack2 FT020T found')
+            logging.info('raw data: ' + sLine)
 	    # Variable Processing from SH unit for WU upload
-            logging.info('Variable processing of SH raw data. \n')
+            logging.info('Variable processing of SH raw data.')
             baro_str = "{0:.2f}".format (sense.get_pressure() * 0.0295300)
             # Variable Processing from JSON output from WR2 unit for WU upload
-            logging.info('Variable processing of WR2 raw data. \n')
+            logging.info('Variable processing of WR2 raw data.')
             raw_data = json.loads(sLine)
             time_stamp = (raw_data['time'])
 
@@ -262,7 +291,7 @@ while True:
             published_cst = published_time.replace(tzinfo=central)
             published_gmt = published_time.astimezone(utc)
             actual_time_published = published_gmt.strftime('%Y-%m-%d %H:%M:%S')
-            # URL Encode timestamp 
+            # URL Encode timestamp
             time_str = (urllib.parse.quote(actual_time_published, safe=''))
             # Format process weather variables into strings for  upload
             humidity_str = "{0:.0f}".format(raw_data['humidity'])
@@ -284,8 +313,9 @@ while True:
             sense.show_message(shMsg, text_colour=[255, 255, 0], back_colour=[0, 0, 102])
             # clear the screen
             sense.clear()
+
             # Form URL into WU format and Send
-            r= requests.get(
+            wur= requests.get(
                 WUurl +
                 WUcreds +
                 "&dateutc=" + time_str + #Formatted time stamp from WR2, comment line out to use "now" option for WU
@@ -300,27 +330,64 @@ while True:
                 "&uv=" + uv_str +
                 "&baromin=" + baro_str +
                 "&softwaretype=" + "Pi3-SH-WR2-Updater" +
-                action_str)
-            # Show a copy of what you formed up and are uploading in HRF 
-            logging.info(WUurl +
-                WUcreds +
-		#date_str+
-                "&dateutc=" + time_str +
-                "&tempf=" + temp_str +
-                "&humidity=" + humidity_str +
-                "&dewptf=" + dewpt_str +
-                "&winddir=" + winddir_str  +
-                "&windspeedmph" + avewind_str +
-                "&windgustmph=" + gustwind_str +
-                "&dailyrainin=" + cumrain_str +
-                "&uv=" + uv_str +
-                "&baromin=" + baro_str +
-                "&softwaretype=" + "Pi3-SH-WR2-Updater" +
-                action_str)
+                WUaction_str)
+
+            # Check upload time against interval
+            # get the current minute
+            current_minute = dt.datetime.now().minute
+            logging.info('Current minute: {}'.format(current_minute))
+            # is it the same minute as the last time we checked?
+            # this will always be true the first time through this loop
+            if current_minute != last_minute:
+                # reset last_minute to the current_minute
+                last_minute = current_minute
+                # is minute zero, or divisible by 10?
+                # we're only going to use measurements every MEASUREMENT_INTERVAL minutes
+                if (current_minute == 0) or ((current_minute % PWS_INTERVAL) == 0):
+                    # get the reading timestamp
+                    now = dt.datetime.now()
+                    logging.info("%d minute mark (%d @ %s)" % (PWS_INTERVAL, current_minute, str(now)))
+                    # Form URL into PWS format and Send
+                    pwsr= requests.get(
+                        PWSurl +
+                        PWScreds +
+                        "&dateutc=" + time_str + #Formatted time stamp from WR2, comment line out to use "now" option for WU
+                        #date_str +  #Use as a replacement for actual time stamp uncomment to use "now" function of WU
+                        "&tempf=" + temp_str +
+                        "&humidity=" + humidity_str +
+                        "&dewptf=" + dewpt_str +
+                        "&winddir=" + winddir_str  +
+                        "&windspeedmph=" + avewind_str +
+                        "&windgustmph=" + gustwind_str +
+                        "&dailyrainin=" + cumrain_str +
+                        "&uv=" + uv_str +
+                        "&baromin=" + baro_str +
+                        "&softwaretype=" + "Pi3-SH-WR2-Updater" +
+                        PWSaction_str)
+                    # Check PWS Feed Status
+                    logging.info('PWS Received ' + str(pwsr.status_code) + '++++++++++++++++++++++++++++++++++')
+            else:
+                logging.info('Skipping PWSweather.com upload-------------------------------------')
+            # Show a copy of what you formed up and are uploading in HRF
+            logging.info('WU URL ' + WUurl)
+            logging.info('WU Creds ' + WUcreds)
+            logging.info('Time Stamp ' + time_str)
+            logging.info('Outdoor Temp ' + temp_str)
+            logging.info('Humidity ' + humidity_str)
+            logging.info('Dew Point ' + dewpt_str)
+            logging.info('Wind Direction ' + winddir_str)
+            logging.info('Wind Speed Ave ' + avewind_str)
+            logging.info('Wind Speed Gust ' + gustwind_str)
+            logging.info('Rain total ' + cumrain_str)
+            logging.info('UV ' + uv_str)
+            logging.info('Barometer' +baro_str)
+            logging.info('Software Pi3-SH-WR2-Updater')
+            logging.info('WU Action ' + WUaction_str)
+
             # Check WU Feed Status
-            print("Received " + str(r.status_code) + " " + str(r.text))
+            logging.info('WU Received ' + str(wur.status_code) + ' ' + str(wur.text))
             # display  green cross for success or a red arrow for fail
-            if (r.status_code == 200):
+            if (wur.status_code == 200):
                 sense.set_pixels(plus)
                 time.sleep(1)
                 sense.clear()
@@ -334,7 +401,7 @@ while True:
                 # increase fail upload count 
                 failct += 1
                 logging.info('Good Upload Count: {}'.format(goodct) + ' Failed Upload Count: {}'.format(failct))
+
     #logging.info('@Interval Pulse Count: {}'.format(pulse))
     #logging.info('Queue Size: {}'.format(queue.qsize()))
-    #time.sleep(MEASUREMENT_INTERVAL) #Uncomment to set an interval for uploads
         sys.stdout.flush()
