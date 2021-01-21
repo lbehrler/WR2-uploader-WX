775df2bff36dc2f7ae0c5d1791713dfd277f43f2
lbehrler <38298678+lbehrler@users.noreply.github.com>
2021-01-21 15:31:36 -0600

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,13 @@ class Config:
   # Weather Underground
   WU_ENABLE = False
   WU_STATION_ID = ""
-  WUP_STATION_KEY = ""
+  WU_STATION_KEY = ""
 
   # PWSweather.com 
   PWS_ENABLE = False
   PWS_STATION_ID = ""
   PWS_STATION_KEY = ""
+  PWS_INTERVAL = 2   # Number of minutes (1-30)
   
   # Sense Hat
   SH_ENABLE = False
