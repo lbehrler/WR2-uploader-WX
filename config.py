b19af7f92024fcd9a0753346b6874d8ff89b1cee
lbehrler <38298678+lbehrler@users.noreply.github.com>
2021-01-21 14:15:50 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 class Config:
   # Weather Underground
+  WU_ENABLE = False
   WU_STATION_ID = ""
   WUP_STATION_KEY = ""
 
   # PWSweather.com 
+  PWS_ENABLE = False
   PWS_STATION_ID = ""
   PWS_STATION_KEY = ""
+  
+  # Sense Hat
+  SH_ENABLE = False
